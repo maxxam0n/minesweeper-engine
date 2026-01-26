3f0a9d60b42df3fa57367d72c0504d97dcb63e84
Maxim Korovin <maximxam0n@gmail.com>
2026-01-26 15:58:00 +0300

    config: fix ESLint configuration syntax
    - Remove non-existent globalIgnores import
    - Use ignores property instead of globalIgnores function
    - Fix spread operator for tseslint.configs.recommended

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,13 @@
 import js from '@eslint/js'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
 
 export default tseslint.config([
-	globalIgnores(['dist']),
 	{
-		files: ['**/*.{ts}'],
-		extends: [js.configs.recommended, tseslint.configs.recommended],
+		ignores: ['dist/**'],
+	},
+	{
+		files: ['**/*.ts'],
+		extends: [js.configs.recommended, ...tseslint.configs.recommended],
 		languageOptions: {
 			ecmaVersion: 2020,
 		},
