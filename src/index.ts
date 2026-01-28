513496dd76ac09b8fd5ecac86446ca8f093adf95
Maxim Korovin <maximxam0n@gmail.com>
2026-01-29 00:31:53 +0300

    index: export new model and solver types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 export { Solver as MinesweeperSolver } from './core/field-solver'
 export { GameEngine as MinesweeperEngine } from './core/game-engine'
 export { IdealSolver as MinesweeperIdealSolver } from './core/ideal-solver'
+export { Cell as FieldCell } from './model/Cell'
+export type { FieldView, SolverConfig } from './model/field-solver.types'
+export type {
+	IdealSolveMetrics,
+	IdealSolverOptions,
+} from './model/ideal-solver.types'
 export * from './model/types'
